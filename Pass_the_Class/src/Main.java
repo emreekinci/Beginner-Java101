e1b197f503ec61ed5b19c18fad3431c823d58830
Emre Ekinci <44337379+emreekinci@users.noreply.github.com>
2022-07-04 02:32:06 +0300

    Add files via upload

---
+++ Pass_the_Class/src/Main.java
@@ -0,0 +1,48 @@
+/*
+Dersler : Matematik, Fizik, Türkçe, Kimya, Müzik
+Geçme Notu : 55
+        Ödev
+Eğer girilen ders notları 0 veya 100 arasında değil ise ortalamaya katılmasın.
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int Math, Physics, Turkish, Chemistry, Music, average ;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Math score : ");
+        Math = scan.nextInt();
+        System.out.println("Physics score : ");
+        Physics = scan.nextInt();
+        System.out.println("Turkish score : ");
+        Turkish = scan.nextInt();
+        System.out.println("Chemistry score : ");
+        Chemistry = scan.nextInt();
+        System.out.println("Music score : ");
+        Music = scan.nextInt();
+
+        // for invalid input values
+        if (Math < 0 || Math > 100)
+            Math = 0;
+        if (Physics < 0 || Physics > 100)
+            Physics = 0;
+        if (Turkish < 0 || Turkish > 100)
+            Turkish = 0;
+        if (Chemistry < 0 || Chemistry > 100)
+            Chemistry = 0;
+        if (Music < 0 || Music > 100)
+            Music = 0;
+
+        average = ( Math + Physics + Turkish + Chemistry + Music ) / 5 ;
+
+        if (average < 55) {
+            System.out.println("You failing the class !!! ");
+        }
+        else {
+            System.out.println("You passed the class, congrats ...");
+        }
+        System.out.println("Your average is : " + average);
+    }
+}
