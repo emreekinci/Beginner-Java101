7792ec0986a087e8dd7260d896230a979136abc6
Emre Ekinci <44337379+emreekinci@users.noreply.github.com>
2022-07-04 19:45:50 +0300

    Add files via upload

---
+++ Sorting_Numbers/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        float num_1, num_2, num_3, max= 0;
+
+        // sorting numbers : descending
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Please enter first number: ");
+        num_1 = scanner.nextFloat();
+
+        System.out.println("Please enter second number: ");
+        num_2 = scanner.nextFloat();
+
+        System.out.println("Please enter third number: ");
+        num_3 = scanner.nextFloat();
+
+        if ((num_1 > num_2) && (num_1 > num_3)) {
+            if (num_2 > num_3) {
+                System.out.print("num1>num2>num3");
+            } else {
+                System.out.print("num1>num3>num2");
+            }
+        } else if ((num_2 > num_1) && (num_2 > num_3)) {
+            if (num_1 > num_3) {
+                System.out.print("num2>num1>num3");
+            }else {
+                System.out.print("num2>num3>num1");
+            }
+
+        } else{
+            if (num_1 > num_2){
+                System.out.print("c>a>b");
+            }else{
+                System.out.print("c>b>a");
+            }
+        }
+    }
+}
+
+
---
+++ Sorting_Numbers/src/readme.md
@@ -0,0 +1,6 @@
+# Ödev
+
+Girilen 3 sayıyı "küçükten büyüğe" sıralayan programı yazınız.
+
+### Linkler
+> <a href="https://app.patika.dev/emrevaljean" target="_blank">Patika Profilim için</a>
