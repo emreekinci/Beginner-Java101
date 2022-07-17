c2984fb8d87a390e2b1e561dd14daf74e48196d3
Emre Ekinci <44337379+emreekinci@users.noreply.github.com>
2022-07-17 15:26:53 +0300

    Update Test.java

--- OOP_3_Constructor/src/Test.java
+++ OOP_3_Constructor/src/Test.java
@@ -2,8 +2,23 @@
 public class Test {
 
     public static void main(String[] args) {
+        
+        /* without constructors
+        Account account1 = new Account();
+        
+        account1.setHesapNo("1234");
+        account1.setBakiye(2000);
+        account1.setIsim("Emre Ekinci");
+        account1.setEmail("asdasd@gmail.com");
+        account1.setTelefonNo("646646468434\n");
 
-        // Account account1 = new Account();
+        System.out.println("Hesabi : "+account1.getHesapNo());
+        System.out.println("Bakiye : "+account1.getBakiye());
+        System.out.println("Isim : "+account1.getIsim());
+        System.out.println("Mail : "+account1.getEmail());
+        System.out.println("Telefon no : "+account1.getTelefonNo());
+
+         */
 
         Account account2 = new Account("123213",1000.0,"Emre Ekinci","emreekincivaljean@gmail.con","324324");
 
